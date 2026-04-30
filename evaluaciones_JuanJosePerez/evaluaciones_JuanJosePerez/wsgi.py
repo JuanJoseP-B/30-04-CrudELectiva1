7ca8e0002f421f947894447b5b7c3fa6f24c0bbb
JuanJoseP-B <jjperezbravo123@gmail.com>
2026-04-30 12:11:00 -0500

    Fix SyntaxError en wsgi.py

--- evaluaciones_JuanJosePerez/evaluaciones_JuanJosePerez/wsgi.py
+++ evaluaciones_JuanJosePerez/evaluaciones_JuanJosePerez/wsgi.py
@@ -5,6 +5,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
